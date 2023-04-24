60a4e0f39a4c1c1c57f722913735102733cacdfb
Matheus Braúna <mattsilvahenrique102@gmail.com>
2023-04-24 19:57:52 -0300

    feat: create find many use case

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,10 +3,11 @@ import { DatabaseModule } from '../database/database.module';
 import { UsersController } from './controllers/users.controller';
 import { CreateUserUseCase } from 'src/@app/use-cases/create-user-use-case';
 import { DeleteUserUseCase } from 'src/@app/use-cases/delete-user-use-case';
+import { FindManyUseCase } from 'src/@app/use-cases/find-many-users-use-case';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [UsersController],
-  providers: [CreateUserUseCase, DeleteUserUseCase],
+  providers: [CreateUserUseCase, DeleteUserUseCase, FindManyUseCase],
 })
 export class HttpModule {}
